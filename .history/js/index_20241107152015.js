6c6323ab0f39306bf23cdbe4e67c6cfd69e4b13d
dinhtri123 <phamdinhtriqs111@gmail.com>
2024-11-08 11:56:04 +0700

    homepage & profile

---
+++ .history/js/index_20241107152015.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function()  {
+    const btnMenu = document.querySelector(".header-icon-menu");
+    const btnCloseMenu = document.querySelector('.header-btn-close')
+    const headerMenu = document.querySelector(".header-menu-wrapper");
+
+    btnMenu.addEventListener('click', () => {
+        headerMenu.classList.add('active')
+    })
+
+    btnCloseMenu.addEventListener("click", () => {
+      headerMenu.classList.remove("active");
+    });
+
+
+    // popup
+    const popup = document.querySelector('.popup');
+    const popupBtnClose = document.querySelector('.popup-btn-close');
+    const btnRegister = document.querySelector(".btn-register");
+    const btnLogin = document.querySelector(".btn-login");
+    btnRegister.addEventListener("click", function () {
+      popup.classList.add("active");
+    });
+    popupBtnClose.addEventListener('click', function() {
+      popup.classList.remove('active')
+    })
+})

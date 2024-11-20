48a5446fde09e727abe5adcd75393a383c13e47e
dinhtri123 <phamdinhtriqs111@gmail.com>
2024-11-20 16:42:43 +0700

    fix css & js  update design

--- js/index.js
+++ js/index.js
@@ -10,33 +10,34 @@ document.addEventListener("DOMContentLoaded", function () {
   btnCloseMenu.addEventListener("click", () => {
     headerMenu.classList.remove("active");
   });
-  const homePage = document.querySelector(".homepage");
-  if (homePage) {
-    // popup
-    const popupRegister = document.querySelector(".popup-register");
-    const popupLogin = document.querySelector(".popup-login");
-    const btnCloseRegister = popupRegister.querySelector(".popup-btn-close");
-    const btnCloseLogin = popupLogin.querySelector(".popup-btn-close");
-    const btnRegister = Array.from(document.querySelectorAll(".btn-register"));
-    const btnLogin = Array.from(document.querySelectorAll(".btn-login"));
-    btnRegister.map(item => {
-      item.addEventListener("click", function () {
-        popupRegister.classList.add("active");
-        popupLogin.classList.remove("active");
-      });
+  // popup
+  const popupRegister = document.querySelector(".popup-register");
+  const popupLogin = document.querySelector(".popup-login");
+  const btnClose = Array.from(
+    document.querySelectorAll(".popup-btn-close")
+  );
+  const btnRegister = Array.from(document.querySelectorAll(".btn-register"));
+  const btnLogin = Array.from(document.querySelectorAll(".btn-login"));
+  btnClose.map(item => {
+    const parentPopup = item.parentNode.parentNode;
+    item.addEventListener('click', function() {
+      parentPopup.classList.remove('active')
     })
-    btnCloseRegister.addEventListener("click", function () {
-      popupRegister.classList.remove("active");
-    });
-    btnLogin.map(item => {
-      item.addEventListener("click", function () {
-        popupLogin.classList.add("active");
-        popupRegister.classList.remove("active");
-      });
-    })
-    btnCloseLogin.addEventListener("click", function () {
+  })
+  btnRegister.map((item) => {
+    item.addEventListener("click", function () {
+      popupRegister.classList.add("active");
       popupLogin.classList.remove("active");
     });
+  });
+  btnLogin.map((item) => {
+    item.addEventListener("click", function () {
+      popupLogin.classList.add("active");
+      popupRegister.classList.remove("active");
+    });
+  });
+  const homePage = document.querySelector(".homepage");
+  if (homePage) {
     // check login
     const headerBtn = document.querySelector(".header-btn");
     const notLogged = Array.from(document.querySelectorAll(".not-logged"));
@@ -103,26 +104,24 @@ document.addEventListener("DOMContentLoaded", function () {
   const careers = document.querySelector(".careers");
   if (careers) {
     const btnMore = Array.from(document.querySelectorAll(".btn-more button"));
-    
-    btnMore.map(item => {
+
+    btnMore.map((item) => {
       item.addEventListener("click", function () {
         const exploreItemContent = item.parentNode.parentNode;
-        if(item.textContent == "Thu gọn") {
-          item.textContent = "Xem thêm"
-          exploreItemContent.style.maxHeight = '145px'
-        }else {
-          item.textContent = "Thu gọn"
-          exploreItemContent.style.maxHeight = '100%'
-
+        if (item.textContent == "Thu gọn") {
+          item.textContent = "Xem thêm";
+          exploreItemContent.classList.remove("active");
+        } else {
+          item.textContent = "Thu gọn";
+          exploreItemContent.classList.add("active");
         }
-        
-      })
-    })
+      });
+    });
   }
 
   // brochure
   const brochure = document.querySelector(".brochure");
-  if(brochure) {
+  if (brochure) {
     const brochureItem = document.querySelector(".brochure-item");
     const brochureListItem = Array.from(
       document.querySelectorAll(".brochure-item-list li a")
@@ -131,39 +130,41 @@ document.addEventListener("DOMContentLoaded", function () {
     brochureListItem.map((item) => {
       item.addEventListener("click", function () {
         brochureItem.classList.add("hidden");
-        file.classList.remove('hidden')
+        file.classList.remove("hidden");
       });
     });
-    file.addEventListener('click', handleClick);
-    function handleClick () {
-      file.classList.add('active');
+    file.addEventListener("click", handleClick);
+    function handleClick() {
+      file.classList.add("active");
       file.removeEventListener("click", handleClick);
     }
   }
 
   // excercise
   const excercise = document.querySelector("#excercise");
-  if(excercise){
+  if (excercise) {
     const percent = document.querySelector(".excercise-progress .percent");
     const progressBg = document.querySelector(
       ".excercise-progress .progress-bg"
     );
     const radio = Array.from(
-      document.querySelectorAll(".excercise-item-option label input[type='radio']")
+      document.querySelectorAll(
+        ".excercise-item-option label input[type='radio']"
+      )
     );
-    
+
     radio.forEach((item) => {
       item.addEventListener("change", updateProgress);
     });
     const totalItems = radio.length;
-    function updateProgress () {
+    function updateProgress() {
       let selectedCount = new Set(
         radio.filter((radio) => radio.checked).map((radio) => radio.name)
       ).size;
 
       let percentage = Math.round((selectedCount / totalItems) * 100);
 
-      percent.textContent = `${percentage* 2}%`;
+      percent.textContent = `${percentage * 2}%`;
       progressBg.style.width = `${percentage * 2}%`;
     }
   }
